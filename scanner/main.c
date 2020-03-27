3ec357dd6c0da29f0e6de706f59317448fb73cfa
Bruno Sampaio <brunosampaioleite@gmail.com>
2020-03-27 17:47:23 -0300

    Syntax tree and symbol table now print to file

--- scanner/main.c
+++ scanner/main.c
@@ -72,7 +72,7 @@ int main( int argc, char * argv[] )
   }
   fprintf(errorfile, "\\----------------------------------\\\n");
   fprintf(errorfile, "ERROS SINTATICOS DO ARQUIVO %s:\n\n", pgm);
-  listing = stdout; /* send listing to screen */
+  listing = fopen("output.txt", "w"); //Print syntax tree and symbol table to file output.txt
   fprintf(listing,"\nCompilação C-: %s\n",pgm);
 #if NO_PARSE
   while (getToken()!=ENDFILE);
